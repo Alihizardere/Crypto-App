08257f05c83fdb8d5e72ef864c25026924b53eaa
Ali Hızardere <aliduyguhzr@gmail.com>
2024-05-04 22:14:28 +0300

    Project created and folders added

---
+++ Crypto-App/Controller/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Crypto-App
+//
+//  Created by alihizardere on 4.05.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
